6c8dcbaa7eb2adddcc3bad0ce7a5b52c43a01dac
hwangyu.kim <skykhk01@gmail.com>
2023-06-02 10:10:26 +0900

    Update basebll feature

--- Sample-Test1/Baseball_test.cpp
+++ Sample-Test1/Baseball_test.cpp
@@ -1,6 +1,31 @@
 #include "pch.h"
 #include "../Baseball/Baseball.cpp"
 
-TEST(BaseballGame, TryGameTest) {
-  EXPECT_EQ(1, 1);
+TEST(BaseballGame, InvalidArguement)
+{
+	Baseball baseball("123");
+	EXPECT_THROW(baseball.guess(""), std::invalid_argument);
+	EXPECT_THROW(baseball.guess("1543"), std::invalid_argument);
+	EXPECT_THROW(baseball.guess("1v3"), std::invalid_argument);
+	EXPECT_THROW(baseball.guess("121"), std::invalid_argument);
+}
+
+TEST(BaseballGame, PerfectMatch)
+{
+	Baseball baseball("123");
+	Result result = baseball.guess("123");
+
+	EXPECT_EQ(true, result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
+}
+
+TEST(BaseballGame, Mismatch)
+{
+	Baseball baseball("123");
+	Result result = baseball.guess("789");
+
+	EXPECT_EQ(false, result.solved);
+	EXPECT_EQ(0, result.strikes);
+	EXPECT_EQ(0, result.balls);
 }
